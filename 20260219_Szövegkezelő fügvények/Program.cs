ddbe8c199bb6559df634af36fa39771c051efbdc
dpecsi <pecsi.daniel@students.jedlik.eu>
2026-02-19 16:01:59 +0100

    félidő

--- 20260219_Szövegkezelő fügvények/Program.cs
+++ 20260219_Szövegkezelő fügvények/Program.cs
@@ -22,12 +22,69 @@
             lista1.Add(10);
             lista1.Add(20);
             lista1.Add(1);
+            lista1.Add(6);
+            lista1.Add(8);
+            lista1.Add(4);
+            lista1.Add(7);
+            lista1.Add(3);
 
             List<int> lista2 = [10, 20, 1];
             List<int> lista3 = new List<int>() { 10, 20, 1 };
             List<int> lista4 = new List<int>([10, 20, 1]);
+            List<int> lista5 = new List<int>(tömb1);
+            List<int> lista6 = [.. tömb2];
 
+            tömb1[5] = lista1[2];
+            lista2[0] = tömb2[2];
+
+            int összeg1 = lista1.Sum(); // A lista elemeinek összegét adja vissza
+            int összeg2 = tömb1.Sum(); // A tömb elemeinek összegét adja vissza
+
+            //tömb1[-1] = 5;    // Negatív indexelés nem lehetséges
+            //tömb1[1:5] = 5;   // Szeletelés nem lehetséges
+            lista2 = lista1.Slice(1, 2);
+
+            int db1 = tömb1.Length;
+            int db2 = tömb1.Count();
+            int db3 = lista1.Count();
+
+            int[,] mátrix = new int[10, 10]
+            {
+                { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 },
+                { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 },
+                { 1, 1, 1, 1, 4, 1, 1, 1, 1, 1 },
+                { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 },
+                { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 },
+                { 1, 1, 1, 1, 1, 1, 0, 1, 1, 1 },
+                { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 },
+                { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 },
+                { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 },
+                { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 },
+            };
+
+            int szám = mátrix[2, 4]; // A mátrix 3. sorában és 5. oszlopában lévő értéket adja ki
             Console.WriteLine(x);
+            Console.WriteLine(mátrix);
+            Console.WriteLine(lista1);
+
+            var szöveg1 = lista1;
+            var szöveg2 = "lista1";
+            foreach (int listaEleme in lista1)
+            {
+                Console.WriteLine(listaEleme);
+            }
+
+            for (int index = 0; index < lista2.Count(); index++)
+            {
+                int listaEleme2 = lista2[index];
+                Console.WriteLine(listaEleme2);
+            }
+
+            lista3.ForEach(listaEleme3 => Console.WriteLine(listaEleme3));
+
+            lista4.ForEach(listaEleme4 => {
+                Console.WriteLine(listaEleme4);
+            });
         }
     }
 }
